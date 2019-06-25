8b711829b00bbd62376665d5ef808f0a7a279bb5
LaMarche05 <adrien.marchand1@gmail.com>
2019-06-25 11:35:42 +0200

    imgui-test: Refactor main so it can compile without dx11 and move app code one folder above, since it's not win32/dx11-specific anymore

--- app/main.cpp
+++ app/main.cpp
@@ -54,23 +54,114 @@ static inline void DebugCrtDumpLeaks()
 //-------------------------------------------------------------------------
 
 #include "imgui.h"
+#include <stdio.h>
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui_te_core.h"
+#include "imgui_te_util.h"
+#include "imgui_tests.h"
+
+#ifdef IMGUI_TESTS_ENABLE_DX11
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx11.h"
 #include <d3d11.h>
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
 #include <tchar.h>
-#include <stdio.h>
-#include <conio.h>
+#endif // IMGUI_TESTS_ENABLE_DX11
 
-#define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui_te_core.h"
-#include "imgui_te_util.h"
-#include "imgui_tests.h"
+//-------------------------------------------------------------------------
+// Test Application
+//-------------------------------------------------------------------------
+
+enum ImGuiBackend
+{
+    ImGuiBackend_Null,
+    ImGuiBackend_DX11,
+};
+
+#ifdef IMGUI_TESTS_ENABLE_DX11
+const ImGuiBackend DEFAULT_BACKEND = ImGuiBackend_DX11;
+const bool DEFAULT_OPT_GUI = true;
+#else
+const ImGuiBackend DEFAULT_BACKEND = ImGuiBackend_Null;
+const bool DEFAULT_OPT_GUI = false;
+#endif
+
+struct TestApp
+{
+    bool                    Quit = false;
+    ImGuiTestEngine*        TestEngine = NULL;
+    ImGuiBackend            Backend = DEFAULT_BACKEND;
+    bool                    OptGUI = DEFAULT_OPT_GUI;
+    bool                    OptFast = true;
+    ImGuiTestVerboseLevel   OptVerboseLevel = ImGuiTestVerboseLevel_COUNT; // Set in main.cpp
+    bool                    OptNoThrottle = false;
+    bool                    OptPauseOnExit = true;
+    char*                   OptFileOpener = NULL;
+    ImVector<char*>         Tests;
+    ImU64                   LastTime = 0;
+};
+
+TestApp g_App;
+
+// User state
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+static bool MainLoopEndFrame()
+{
+    ImGuiTestEngine_ShowTestWindow(g_App.TestEngine, NULL);
+
+    static bool show_demo_window = true;
+    static bool show_another_window = false;
+
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    ImGui::EndFrame();
+
+    return true;
+}
+
 
 //-------------------------------------------------------------------------
 // DX11 Stuff
 //-------------------------------------------------------------------------
+
+#ifdef IMGUI_TESTS_ENABLE_DX11
 
 static ID3D11Device*            g_pd3dDevice = NULL;
 static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
@@ -156,38 +247,6 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-// User state
-static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-static bool show_demo_window = true;
-static bool show_another_window = false;
-
-//-------------------------------------------------------------------------
-// Test Application
-//-------------------------------------------------------------------------
-
-enum ImGuiBackend
-{
-    ImGuiBackend_Null,
-    ImGuiBackend_DX11
-};
-
-struct TestApp
-{
-    bool                    Quit            = false;
-    ImGuiTestEngine*        TestEngine      = NULL;
-    ImGuiBackend            Backend         = ImGuiBackend_DX11;
-    bool                    OptGUI          = true;
-    bool                    OptFast         = true;
-    ImGuiTestVerboseLevel   OptVerboseLevel = ImGuiTestVerboseLevel_COUNT; // Set in main.cpp
-    bool                    OptNoThrottle   = false;
-    bool                    OptPauseOnExit  = true;
-    char*                   OptFileOpener   = NULL;
-    ImVector<char*>         Tests;
-    ImU64                   LastTime = 0;
-};
-
-TestApp g_App;
-
 static bool OsCreateProcess(const char* cmd_line)
 {
     STARTUPINFOA siStartInfo;
@@ -222,30 +281,30 @@ static void FileOpenerFunc(const char* filename, int line, void*)
 
 bool MainLoopNewFrameDX11()
 {
-	MSG msg;
-	ZeroMemory(&msg, sizeof(msg));
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
 
-	// Poll and handle messages (inputs, window resize, etc.)
-	// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-	// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-	// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-	// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-	while (true)
-	{
-		if (!PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-			break;
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+    // Poll and handle messages (inputs, window resize, etc.)
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    while (true)
+    {
+        if (!PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+            break;
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
         if (msg.message == WM_QUIT)
         {
             g_App.Quit = true;
             ImGuiTestEngine_Abort(g_App.TestEngine);
             break;
         }
-		continue;
-	}
+        continue;
+    }
 
-	// Start the Dear ImGui frame
+    // Start the Dear ImGui frame
     if (!g_App.Quit)
     {
         ImGui_ImplDX11_NewFrame();
@@ -259,10 +318,110 @@ bool MainLoopNewFrameDX11()
         //ImGui::NewFrame();
     }
 
-	return !g_App.Quit;
+    return !g_App.Quit;
 }
 
-bool MainLoopNewFrameNull()
+static void MainLoopEndFrameRenderDX11()
+{
+    // Rendering
+    ImGui::Render();
+
+    // Update and Render additional Platform Windows
+#ifdef IMGUI_HAS_VIEWPORT
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+    }
+#endif
+    g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+    g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+    ImGuiTestEngineIO& test_io = ImGuiTestEngine_GetIO(g_App.TestEngine);
+    if ((test_io.RunningTests && test_io.ConfigRunFast) || test_io.ConfigNoThrottle)
+        g_pSwapChain->Present(0, 0); // Present without vsync
+    else
+        g_pSwapChain->Present(1, 0); // Present with vsync
+}
+
+static void MainLoopDX11()
+{
+    // Initialize back-end
+    HWND hwnd = 0;
+    WNDCLASSEX wc = { 0 };
+
+    // Create application window
+    wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
+    RegisterClassEx(&wc);
+    hwnd = CreateWindow(_T("ImGui Example"), _T("Dear ImGui - Tests"), WS_OVERLAPPEDWINDOW, 60, 60, 1280 + 160, 800 + 100, NULL, NULL, wc.hInstance, NULL);
+
+    // Initialize Direct3D
+    if (CreateDeviceD3D(hwnd) < 0)
+    {
+        CleanupDeviceD3D();
+        UnregisterClass(_T("ImGui Example"), wc.hInstance);
+        return;
+    }
+
+    // Show the window
+    ShowWindow(hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(hwnd);
+
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+    ImGuiTestEngineIO& test_io = ImGuiTestEngine_GetIO(g_App.TestEngine);
+    //test_io.ConfigLogToTTY = true;
+    //test_io.ConfigVerboseLevel = ImGuiTestVerboseLevel_Max;
+
+    auto end_frame_func =
+        [](ImGuiTestEngine*, void*)
+        {
+            if (!MainLoopEndFrame())
+                return false;
+#if 0
+            // Super fast mode doesn't render/present
+            if (test_io.RunningTests && test_io.ConfigRunFast)
+                return true;
+#endif
+            MainLoopEndFrameRenderDX11();
+            return true;
+        };
+
+    test_io.NewFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopNewFrameDX11(); };
+    test_io.EndFrameFunc = end_frame_func;
+
+    test_io.FileOpenerFunc = FileOpenerFunc;
+
+    while (1)
+    {
+        if (!MainLoopNewFrameDX11())
+            break;
+
+        if (!end_frame_func(NULL, NULL))
+            break;
+    }
+
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceD3D();
+    DestroyWindow(hwnd);
+    UnregisterClass(_T("ImGui Example"), wc.hInstance);
+}
+
+#else
+static void MainLoopDX11() {}
+#endif // IMGUI_TESTS_ENABLE_DX11
+
+//-------------------------------------------------------------------------
+// No GUI Stuff
+//-------------------------------------------------------------------------
+
+static bool MainLoopNewFrameNull()
 {
     ImGuiIO& io = ImGui::GetIO();
     io.DisplaySize = ImVec2(1920, 1080);
@@ -284,83 +443,32 @@ bool MainLoopNewFrameNull()
     return true;
 }
 
-bool MainLoopEndFrame()
+static void MainLoopNull()
 {
+    // Init
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->Build();
+    for (int n = 0; n < ImGuiKey_COUNT; n++)
+        io.KeyMap[n] = n;
+
     ImGuiTestEngineIO& test_io = ImGuiTestEngine_GetIO(g_App.TestEngine);
-    ImGuiTestEngine_ShowTestWindow(g_App.TestEngine, NULL);
+    test_io.ConfigLogToTTY = true;
+    test_io.NewFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopNewFrameNull(); };
+    test_io.EndFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopEndFrame(); };
+    test_io.FileOpenerFunc = NULL;
 
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    while (1)
     {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        ImGui::End();
+        if (!MainLoopNewFrameNull())
+            break;
+        if (!MainLoopEndFrame())
+            break;
     }
 
-    // 3. Show another simple window.
-    if (show_another_window)
-    {
-        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
-        ImGui::End();
-    }
+    ImGuiTestEngine_PrintResultSummary(g_App.TestEngine);
 
-    ImGui::EndFrame();
-
-#if 0
-    // Super fast mode doesn't render/present
-    if (test_io.RunningTests && test_io.ConfigRunFast)
-        return true;
-#endif
-
-    // Rendering
-    if (g_App.Backend == ImGuiBackend_DX11)
-    {
-        ImGui::Render();
-
-        // Update and Render additional Platform Windows
-#ifdef IMGUI_HAS_VIEWPORT
-        ImGuiIO& io = ImGui::GetIO();
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-        {
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-        }
-#endif
-
-        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
-        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
-        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-
-        if ((test_io.RunningTests && test_io.ConfigRunFast) || test_io.ConfigNoThrottle)
-            g_pSwapChain->Present(0, 0); // Present without vsync
-        else
-            g_pSwapChain->Present(1, 0); // Present with vsync
-    }
-
-    return true;
-};
+    ImGui::DestroyContext();
+}
 
 static bool ParseCommandLineOptions(int argc, char const** argv)
 {
@@ -474,29 +582,6 @@ int main(int argc, char const** argv)
     if (g_App.OptGUI == false)
         g_App.Backend = ImGuiBackend_Null;
 
-    // Initialize back-end
-    HWND hwnd = 0;
-    WNDCLASSEX wc = { 0 };
-    if (g_App.Backend == ImGuiBackend_DX11)
-    {
-        // Create application window
-        wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
-        RegisterClassEx(&wc);
-        hwnd = CreateWindow(_T("ImGui Example"), _T("Dear ImGui - Tests"), WS_OVERLAPPEDWINDOW, 60, 60, 1280+160, 800+100, NULL, NULL, wc.hInstance, NULL);
-
-        // Initialize Direct3D
-        if (CreateDeviceD3D(hwnd) < 0)
-        {
-            CleanupDeviceD3D();
-            UnregisterClass(_T("ImGui Example"), wc.hInstance);
-            return 1;
-        }
-
-        // Show the window
-        ShowWindow(hwnd, SW_SHOWDEFAULT);
-        UpdateWindow(hwnd);
-    }
-
     // Setup Dear ImGui binding
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -560,59 +645,11 @@ int main(int argc, char const** argv)
     switch (g_App.Backend)
     {
     case ImGuiBackend_Null:
-    {
-        // Init
-        io.Fonts->Build();
-        for (int n = 0; n < ImGuiKey_COUNT; n++)
-            io.KeyMap[n] = n;
-        test_io.ConfigLogToTTY = true;
-        test_io.NewFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopNewFrameNull(); };
-        test_io.EndFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopEndFrame(); };
-        test_io.FileOpenerFunc = FileOpenerFunc;
-
-        while (1)
-        {
-            if (!MainLoopNewFrameNull())
-                break;
-            if (!MainLoopEndFrame())
-                break;
-        }
-
-        ImGuiTestEngine_PrintResultSummary(g_App.TestEngine);
-
-        ImGui::DestroyContext();
+        MainLoopNull();
         break;
-    }
     case ImGuiBackend_DX11:
-    {
-        ImGui_ImplWin32_Init(hwnd);
-        ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
-
-        //test_io.ConfigLogToTTY = true;
-        //test_io.ConfigVerboseLevel = ImGuiTestVerboseLevel_Max;
-
-        test_io.NewFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopNewFrameDX11(); };
-        test_io.EndFrameFunc = [](ImGuiTestEngine*, void*) { return MainLoopEndFrame(); };
-        test_io.FileOpenerFunc = FileOpenerFunc;
-
-        while (1)
-        {
-            if (!MainLoopNewFrameDX11())
-                break;
-            if (!MainLoopEndFrame())
-                break;
-        }
-
-        ImGui_ImplDX11_Shutdown();
-        ImGui_ImplWin32_Shutdown();
-        ImGui::DestroyContext();
-
-        CleanupDeviceD3D();
-        DestroyWindow(hwnd);
-        UnregisterClass(_T("ImGui Example"), wc.hInstance);
-
+        MainLoopDX11();
         break;
-    }
     }
 
     ImGuiTestEngine_ShutdownContext(g_App.TestEngine);
--- imconfig_tests.h
+++ imconfig_tests.h
@@ -3,6 +3,10 @@
 #define IMGUI_ENABLE_TEST_ENGINE    1
 #define ImDrawIdx                   unsigned int
 
+#ifdef _WIN32
+#define IMGUI_TESTS_ENABLE_DX11
+#endif
+
 // Assert handler
 extern void ImGuiTestEngineHook_AssertFunc(const char* expr, const char* file, const char* function, int line);
 #define IM_ASSERT(_EXPR)            do { !!(_EXPR) || (ImGuiTestEngineHook_AssertFunc(#_EXPR, __FILE__, __func__, __LINE__),true); } while (0)
