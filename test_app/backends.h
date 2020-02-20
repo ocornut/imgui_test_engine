5d01788fc9f2e93675eb0e03cd64df511826a1fc
Rokas Kupstys <rokups@zoho.com>
2020-02-20 16:42:32 +0200

    TestApp: Remove old null backend code. It is in imgui_app.cpp now.

--- test_app/backends.h
+++ test_app/backends.h
@@ -1,9 +1,3 @@
-
-void    MainLoop();
-void    MainLoopNull();
-bool    CaptureFramebufferScreenshot(int x, int y, int w, int h, unsigned int* pixels, void* user_data);
-bool    CaptureScreenshotNull(int x, int y, int w, int h, unsigned int* pixels, void* user_data);
-
 // Coroutine implementation using std::thread
 ImGuiTestCoroutineHandle ImCoroutineCreate(ImGuiTestCoroutineFunc func, const char* name, void* ctx);
 void    ImCoroutineDestroy(ImGuiTestCoroutineHandle handle);
