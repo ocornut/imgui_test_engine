df06c6d3d02fe3181a342a3e031e2e7a53fbf4ec
ocornut <omarcornut@gmail.com>
2020-03-10 19:53:02 +0100

    Test Engine: Added imgui_te_imconfig.h for easy usage for third party apps.

--- test_app/imconfig_test_app.h
+++ test_app/imconfig_test_app.h
@@ -1,12 +1,2 @@
 #include "imconfig.h"
-//#define IMGUI_USE_FNV1A
-#define IMGUI_ENABLE_TEST_ENGINE
-#define ImDrawIdx                   unsigned int
-
-#ifdef _WIN32
-#define IMGUI_TESTS_BACKEND_WIN32_DX11
-#endif
-
-// Assert handler
-extern void ImGuiTestEngineHook_AssertFunc(const char* expr, const char* file, const char* function, int line);
-#define IM_ASSERT(_EXPR)            do { !!(_EXPR) || (ImGuiTestEngineHook_AssertFunc(#_EXPR, __FILE__, __func__, __LINE__),true); } while (0)
+#include "test_engine/imgui_te_imconfig.h"
---
+++ test_engine/imgui_te_imconfig.h
@@ -0,0 +1,8 @@
+// #include this file in your imconfig.h to enable test engine.
+
+// Compile Dear ImGui with test engine hooks
+#define IMGUI_ENABLE_TEST_ENGINE
+
+// Bind assert macro
+extern void ImGuiTestEngineHook_AssertFunc(const char* expr, const char* file, const char* func, int line);
+#define IM_ASSERT(_EXPR)    do { !!(_EXPR) || (ImGuiTestEngineHook_AssertFunc(#_EXPR, __FILE__, __func__, __LINE__), true); } while (0)
