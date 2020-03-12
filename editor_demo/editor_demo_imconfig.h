5ddbba8f4521a047843b1284f5870260cffcb938
ocornut <omarcornut@gmail.com>
2020-03-12 13:47:44 +0100

    Fix includes from imconfig files to facilitate integration with build systems

--- editor_demo/editor_demo_imconfig.h
+++ editor_demo/editor_demo_imconfig.h
@@ -1,6 +1,7 @@
 #include "imconfig.h"
 
+// Use relative path as this file may be compiled with different settings
 #ifdef EDITOR_DEMO_ENABLE_TEST_ENGINE
-#include "test_engine/imgui_te_imconfig.h"
+#include "../test_engine/imgui_te_imconfig.h"
 #endif
 
--- test_app/test_app_imconfig.h
+++ test_app/test_app_imconfig.h
@@ -1,2 +1,4 @@
 #include "imconfig.h"
-#include "test_engine/imgui_te_imconfig.h"
+
+// Use relative path as this file may be compiled with different settings
+#include "../test_engine/imgui_te_imconfig.h"
