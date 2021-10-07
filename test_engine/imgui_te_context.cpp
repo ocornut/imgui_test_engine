ocornut/imgui_test_engine

commit 4222f090da1f9596cc6c38be920ff3726e55c85c

Tests: added "window_title_ctx_menu" testing BeginPopupContextItem() on window titlebar/docked window tab.

test_engine/imgui_te_context.cpp
@@ -1543,6 +1543,52 @@ ImVec2   ImGuiTestContext::GetVoidPos()
     return void_pos;
 }
 
+ImVec2  ImGuiTestContext::GetWindowTitlebarPoint(ImGuiTestRef window_ref)
+{
+    // FIXME-TESTS: Need to find a -visible- click point. drag_pos may end up being outside of main viewport.
+
+    if (IsError())
+        return ImVec2();
+
+    ImGuiWindow* window = NULL;
+    if (!window_ref.IsEmpty())
+        window = GetWindowByRef(window_ref);
+
+    if (window == NULL)
+        if (ImGuiTestItemInfo* item_info = ItemInfo(RefID))
+            window = item_info->Window;
+
+    if (window == NULL)
+    {
+        IM_ERRORF_NOHDR("Unable to locate ref window: '%s'", window_ref.Path);
+        return ImVec2();
+    }
+
+    ImVec2 drag_pos;
+    for (int n = 0; n < 2; n++)
+    {
+#ifdef IMGUI_HAS_DOCK
+        if (window->DockNode != NULL && window->DockNode->TabBar != NULL)
+        {
+            ImGuiTabBar* tab_bar = window->DockNode->TabBar;
+            ImGuiTabItem* tab = ImGui::TabBarFindTabByID(tab_bar, window->ID);
+            IM_ASSERT(tab != NULL);
+            drag_pos = tab_bar->BarRect.Min + ImVec2(tab->Offset + tab->Width * 0.5f, tab_bar->BarRect.GetHeight() * 0.5f);
+        }
+        else
+#endif
+        {
+            const float h = window->TitleBarHeight();
+            drag_pos = ImFloor(window->Pos + ImVec2(window->Size.x, h) * 0.5f);
+        }
+
+        // If we didn't have to teleport it means we can reach the position already
+        if (!WindowTeleportToMakePosVisibleInViewport(window, drag_pos))
+            break;
+    }
+    return drag_pos;
+}
+
 void    ImGuiTestContext::MouseClickOnVoid(int mouse_button)
 {
     ImGuiContext& g = *UiContext;
@@ -2584,31 +2630,7 @@ void    ImGuiTestContext::WindowMove(ImGuiTestRef ref, ImVec2 input_pos, ImVec2 
     WindowBringToFront(window);
     WindowCollapse(window, false);
 
-    // FIXME-TESTS: Need to find a -visible- click point. drag_pos may end up being outside of main viewport.
-    ImVec2 drag_pos;
-    for (int n = 0; n < 2; n++)
-    {
-#ifdef IMGUI_HAS_DOCK
-        if (window->DockNode != NULL && window->DockNode->TabBar != NULL)
-        {
-            ImGuiTabBar* tab_bar = window->DockNode->TabBar;
-            ImGuiTabItem* tab = ImGui::TabBarFindTabByID(tab_bar, window->ID);
-            IM_ASSERT(tab != NULL);
-            drag_pos = tab_bar->BarRect.Min + ImVec2(tab->Offset + tab->Width * 0.5f, tab_bar->BarRect.GetHeight() * 0.5f);
-        }
-        else
-#endif
-        {
-            const float h = window->TitleBarHeight();
-            drag_pos = ImFloor(window->Pos + ImVec2(window->Size.x, h) * 0.5f);
-        }
-
-        // If we didn't have to teleport it means we can reach the position already
-        if (!WindowTeleportToMakePosVisibleInViewport(window, drag_pos))
-            break;
-    }
-
-    MouseMoveToPos(drag_pos);
+    MouseMoveToPos(GetWindowTitlebarPoint(ref));
     //IM_CHECK_SILENT(UiContext->HoveredWindow == window);
     MouseDown(0);
 

test_app/imgui_tests.cpp
@@ -1189,6 +1189,79 @@ void RegisterTests_Window(ImGuiTestEngine* e)
             ctx->Yield();
         }
     };
+
+    // ## Test context menus on window titlebar/docked window tab.
+    t = IM_REGISTER_TEST(e, "window", "window_title_ctx_menu");
+    t->GuiFunc = [](ImGuiTestContext* ctx)
+    {
+        auto& vars = ctx->GenericVars;
+        ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_Appearing);
+        ImGui::Begin("Window A", &vars.BoolArray[0], ImGuiWindowFlags_NoSavedSettings);
+        if (ImGui::BeginPopupContextItem("Popup A"))
+            ImGui::EndPopup();
+        ImGui::TextUnformatted("Window A");
+        ImGui::End();
+#ifdef IMGUI_HAS_DOCK
+        ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_Appearing);
+        ImGui::Begin("Window B", NULL, ImGuiWindowFlags_NoSavedSettings);
+        ImGui::TextUnformatted("Window B");
+        ImGui::End();
+#endif
+    };
+    t->TestFunc = [](ImGuiTestContext* ctx)
+    {
+        ImGuiContext& g = *ctx->UiContext;
+        Str30f popup_name("##Popup_%08x", ctx->GetID("Window A/Popup A"));
+        ImGuiWindow* window = ctx->GetWindowByRef("Window A");
+        ctx->SetRef("Window A");
+#ifdef IMGUI_HAS_DOCK
+        ctx->DockClear("Window A", "Window B", NULL);
+#endif
+        ctx->WindowBringToFront(window);
+
+        // Titlebar of undocked window
+        ctx->MouseMoveToPos(ctx->GetWindowTitlebarPoint("/Window A"));
+        ctx->MouseClick(ImGuiMouseButton_Right);
+        IM_CHECK_NE(g.NavWindow, (ImGuiWindow*)NULL);
+        IM_CHECK_STR_EQ(g.NavWindow->Name, popup_name.c_str());
+        ctx->PopupCloseAll();
+
+        // Close button of undocked window
+        ctx->ItemClick(ctx->GetID("#CLOSE"), ImGuiMouseButton_Right);
+        IM_CHECK_NE(g.NavWindow, (ImGuiWindow*)NULL);
+        IM_CHECK_STR_EQ(g.NavWindow->Name, popup_name.c_str());
+
+        // Collapse button of undocked window
+        ctx->ItemClick(ctx->GetID("#COLLAPSE"), ImGuiMouseButton_Right);
+        IM_CHECK_NE(g.NavWindow, (ImGuiWindow*)NULL);
+        IM_CHECK_STR_EQ(g.NavWindow->Name, popup_name.c_str());
+
+#ifdef IMGUI_HAS_DOCK
+        ctx->DockInto("Window B", "Window A");
+
+        // Tab of docked window
+        ctx->MouseMoveToPos(ctx->GetWindowTitlebarPoint("/Window A"));
+        ctx->MouseClick(ImGuiMouseButton_Right);
+        IM_CHECK_NE(g.NavWindow, (ImGuiWindow*)NULL);
+        IM_CHECK_STR_EQ(g.NavWindow->Name, popup_name.c_str());
+        ctx->PopupCloseAll();
+
+        // Collapse button of docked window tab
+        ctx->ItemClick(ctx->GetID("#CLOSE"), ImGuiMouseButton_Right);
+        IM_CHECK_NE(g.NavWindow, (ImGuiWindow*)NULL);
+        IM_CHECK_STR_EQ(g.NavWindow->Name, popup_name.c_str());
+        ctx->PopupCloseAll();
+
+        // Close button of dock node
+        ctx->ItemClick(ctx->GetID("#CLOSE", window->DockNode->ID), ImGuiMouseButton_Right);
+        IM_CHECK_EQ(g.NavWindow, window->RootWindowDockTree);
+        ctx->PopupCloseAll();
+
+        // Collapse button of dock node
+        ctx->ItemClick(ctx->GetID("#COLLAPSE", window->DockNode->ID), ImGuiMouseButton_Right);
+        IM_CHECK_EQ(g.NavWindow, window->RootWindowDockTree);
+#endif
+    };
 }
 
 

test_engine/imgui_te_context.h
@@ -308,6 +308,7 @@ struct ImGuiTestContext
     ImVec2      GetMainViewportPos();
     ImVec2      GetMainViewportSize();
     ImVec2      GetVoidPos();
+    ImVec2      GetWindowTitlebarPoint(ImGuiTestRef window_ref);                    // Return a clickable point on window titlebar (window tab for docked windows).
 
     // Screen captures
     // - Simple API

commit dea9556a8d2ff1f6b8c90ecc56f5c7974a6cf1c4

Fix GetWindowTitlebarPoint() broken in 4222f090

test_engine/imgui_te_context.cpp
@@ -1550,14 +1550,7 @@ ImVec2  ImGuiTestContext::GetWindowTitlebarPoint(ImGuiTestRef window_ref)
     if (IsError())
         return ImVec2();
 
-    ImGuiWindow* window = NULL;
-    if (!window_ref.IsEmpty())
-        window = GetWindowByRef(window_ref);
-
-    if (window == NULL)
-        if (ImGuiTestItemInfo* item_info = ItemInfo(RefID))
-            window = item_info->Window;
-
+    ImGuiWindow* window = GetWindowByRef(window_ref);
     if (window == NULL)
     {
         IM_ERRORF_NOHDR("Unable to locate ref window: '%s'", window_ref.Path);