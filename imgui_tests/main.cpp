b5ae2cedc6c7658c93a3e5a74b715a946a418875
ocornut <omarcornut@gmail.com>
2022-02-10 16:01:04 +0100

    Tests: revert to use regular looking mouse cursor for now. Ideally we may want to alter this based on state (e.g. capturing screen/video) ? or leave the fancy cursor as a demo

--- imgui_tests/main.cpp
+++ imgui_tests/main.cpp
@@ -579,7 +579,8 @@ int main(int argc, char** argv)
 
 #if IMGUI_VERSION_NUM >= 18701
         if (test_io.RunningTests && !test_io.ConfigMouseDrawCursor)
-            ImGui::RenderMouseCursor(io.MousePos, 1.2f, ImGui::GetMouseCursor(), IM_COL32(255, 255, 120, 255), IM_COL32(0, 0, 0, 255), IM_COL32(0, 0, 0, 60));
+            ImGui::RenderMouseCursor(io.MousePos, 1.0f, ImGui::GetMouseCursor(), IM_COL32_WHITE, IM_COL32_BLACK, IM_COL32(0, 0, 0, 48));
+            //ImGui::RenderMouseCursor(io.MousePos, 1.2f, ImGui::GetMouseCursor(), IM_COL32(255, 255, 120, 255), IM_COL32(0, 0, 0, 255), IM_COL32(0, 0, 0, 60)); // Custom yellow cursor
 #endif
 
         ImGui::Render();
