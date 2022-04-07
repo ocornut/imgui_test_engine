ocornut/imgui_test_engine

commit c432daa47aec2c9e44445f20ba321bf997fd6575

TestEngine: Implement advanced patterns for including/excluding tests.
Only tests satisfying all pattern terms will be included in testing. Special terms "all"/"tests"/"perfs" include tests from respective groups.
By default tests are matched by looking for a substring in entire test name.
Terms prefixed with "^" search for a term at the start of test name.
Terms prefixed with "-" exclude tests matching the term.
Multiple prefixes may be combined in one term.

Also implemented handling of "--" command line parameter which ends submission of options and rest of arguments are treated as test patterns.
Following command line examples are functionally identical:
- ./imgui_tests tests,-window
- ./imgui_tests -- tests -window

imgui_test_engine/imgui_te_engine.cpp
@@ -1142,7 +1142,7 @@ void ImGuiTestEngine_QueueTest(ImGuiTestEngine* engine, ImGuiTest* test, ImGuiTe
 ImGuiTest* ImGuiTestEngine_RegisterTest(ImGuiTestEngine* engine, const char* category, const char* name, const char* src_file, int src_line)
 {
     ImGuiTestGroup group = ImGuiTestGroup_Tests;
-    if (strcmp(category, "perf") == 0)
+    if (strcmp(category, "perfs") == 0)
         group = ImGuiTestGroup_Perfs;
 
     ImGuiTest* t = IM_NEW(ImGuiTest)();
@@ -1161,23 +1161,76 @@ ImGuiPerfTool* ImGuiTestEngine_GetPerfTool(ImGuiTestEngine* engine)
     return engine->PerfTool;
 }
 
+// Filter tests by a specified query. Query is composed of one or more comma-separated filter terms optionally prefixed with modifiers.
+// Available modifiers:
+// - '-' excludes tests matched by the term.
+// - '^' anchors term matching to the start of the string.
+// Example queries:
+// - "all"   : all tests, no matter what group they are in.
+// - "tests" : tests in ImGuiTestGroup_Tests group.
+// - "perfs" : tests in ImGuiTestGroup_Perfs group.
+// - "^nav_" : all tests with name starting with "nav_".
+// - "-xxx"  : no tests will be matched because query does not include any.
+// - "tests,-scroll,-^nav_" : all tests that do not contain "scroll" in their name and does not start with "nav_".
+static bool ImGuiTestEngine_PassFilter(ImGuiTest* test, const char* filter)
+{
+    bool include = false;
+    for (const char* filter_start = filter; filter_start[0];)
+    {
+        // Filter modifiers
+        bool is_exclude = false;
+        bool is_anchor_to_start = false;
+        for (;;)
+        {
+            if (filter_start[0] == '-')
+            {
+                is_exclude = true;
+                filter_start++;
+            }
+            else if (filter_start[0] == '^')
+            {
+                is_anchor_to_start = true;
+                filter_start++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        const char* filter_end = strstr(filter_start, ",");
+        filter_end = filter_end ? filter_end : filter_start + strlen(filter_start);
+
+        if (strncmp(filter_start, "all", filter_end - filter_start) == 0)
+        {
+            include = !is_exclude;
+        }
+        else if (strncmp(filter_start, "tests", filter_end - filter_start) == 0)
+            include = (test->Group == ImGuiTestGroup_Tests) ? !is_exclude : include;
+        else if (strncmp(filter_start, "perfs", filter_end - filter_start) == 0)
+            include = (test->Group == ImGuiTestGroup_Perfs) ? !is_exclude : include;
+        if (is_anchor_to_start && strncmp(test->Name, filter_start, filter_end - filter_start) == 0)
+            include = !is_exclude;
+        else if (!is_anchor_to_start && ImStristr(test->Name, NULL, filter_start, filter_end) != NULL)
+            include = !is_exclude;
+
+        filter_start = filter_end + (filter_end[0] == ',' ? 1 : 0);
+    }
+    return include;
+}
+
 void ImGuiTestEngine_QueueTests(ImGuiTestEngine* engine, ImGuiTestGroup group, const char* filter_str, ImGuiTestRunFlags run_flags)
 {
-    IM_ASSERT(group >= 0 && group < ImGuiTestGroup_COUNT);
-    ImGuiTextFilter filter;
-    if (filter_str != NULL)
-    {
-        IM_ASSERT(strlen(filter_str) + 1 < IM_ARRAYSIZE(filter.InputBuf));
-        ImFormatString(filter.InputBuf, IM_ARRAYSIZE(filter.InputBuf), "%s", filter_str);
-        filter.Build();
-    }
+    IM_ASSERT(group >= ImGuiTestGroup_Unknown && group < ImGuiTestGroup_COUNT);
     for (int n = 0; n < engine->TestsAll.Size; n++)
     {
         ImGuiTest* test = engine->TestsAll[n];
-        if (test->Group != group)
+        if (group != ImGuiTestGroup_Unknown && test->Group != group)
             continue;
-        if (!filter.PassFilter(test->Name))
+
+        if (!ImGuiTestEngine_PassFilter(test, filter_str))
             continue;
+
         ImGuiTestEngine_QueueTest(engine, test, run_flags);
     }
 }

imgui_tests/main.cpp
@@ -165,9 +165,10 @@ static void ShowUI()
 
 static bool ParseCommandLineOptions(int argc, char** argv)
 {
+    bool end_of_options = false;
     for (int n = 1; n < argc; n++)
     {
-        if (argv[n][0] == '-')
+        if (!end_of_options && argv[n][0] == '-')
         {
             // Command-line option
             if (strcmp(argv[n], "-v") == 0)
@@ -251,9 +252,13 @@ static bool ParseCommandLineOptions(int argc, char** argv)
             {
                 g_App.OptExportFilename = argv[n + 1];
             }
+            else if (strcmp(argv[n], "--") == 0)
+            {
+                end_of_options = true;
+            }
             else
             {
-                printf("Syntax: %s <options> [tests]\n", argv[0]);
+                printf("Syntax: %s <options> [tests...]\n", argv[0]);
                 printf("Options:\n");
                 printf("  -h                       : show command-line help.\n");
                 printf("  -v                       : verbose mode (same as -v3 -ve4)\n");
@@ -271,6 +276,8 @@ static bool ParseCommandLineOptions(int argc, char** argv)
                 printf("Tests:\n");
                 printf("   all/tests/perf          : queue by groups: all, only tests, only performance benchmarks.\n");
                 printf("   [pattern]               : queue all tests containing the word [pattern].\n");
+                printf("   [-pattern]              : queue all tests not containing the word [pattern].\n");
+                printf("   [^pattern]              : queue all tests starting with the word [pattern].\n");
                 return false;
             }
         }
@@ -342,26 +349,25 @@ static void QueueTests(ImGuiTestEngine* engine)
         g_App.TestsToRun.push_back(strdup("tests"));
 
     // Queue requested tests
-    // FIXME: Maybe need some cleanup to not hard-coded groups.
     ImGuiTestRunFlags run_flags = ImGuiTestRunFlags_CommandLine;
     if (g_App.OptGuiFunc)
         run_flags |= ImGuiTestRunFlags_GuiFuncOnly;
+
+    // Special groups are supported by ImGuiTestEngine_QueueTests(): "all", "tests", "perfs"
+    // Following command line examples are functionally identical:
+    //  ./imgui_tests tests,-window
+    //  ./imgui_tests -- tests -window
+    // See comments above ImGuiTestEngine_QueueTests() for more details.
+    Str256 filter;
     for (int n = 0; n < g_App.TestsToRun.Size; n++)
     {
         char* test_spec = g_App.TestsToRun[n];
-        if (strcmp(test_spec, "tests") == 0)
-            ImGuiTestEngine_QueueTests(engine, ImGuiTestGroup_Tests, NULL, run_flags);
-        else if (strcmp(test_spec, "perf") == 0)
-            ImGuiTestEngine_QueueTests(engine, ImGuiTestGroup_Perfs, NULL, run_flags);
-        else
-        {
-            if (strcmp(test_spec, "all") == 0)
-                test_spec = NULL;
-            for (int group = 0; group < ImGuiTestGroup_COUNT; group++)
-                ImGuiTestEngine_QueueTests(engine, (ImGuiTestGroup)group, test_spec, run_flags);
-        }
+        if (!filter.empty())
+            filter.append(",");
+        filter.append(test_spec);
         IM_FREE(test_spec);
     }
+    ImGuiTestEngine_QueueTests(engine, ImGuiTestGroup_Unknown, filter.c_str(), run_flags);
     g_App.TestsToRun.clear();
 }
 

commit 0cdd92b7d2838e37bac5ebb759f0b8eaf91e1499

TestEngine: Fix test group assignment
(broken by my amend in c432daa)

imgui_test_engine/imgui_te_engine.cpp
@@ -1142,7 +1142,7 @@ void ImGuiTestEngine_QueueTest(ImGuiTestEngine* engine, ImGuiTest* test, ImGuiTe
 ImGuiTest* ImGuiTestEngine_RegisterTest(ImGuiTestEngine* engine, const char* category, const char* name, const char* src_file, int src_line)
 {
     ImGuiTestGroup group = ImGuiTestGroup_Tests;
-    if (strcmp(category, "perfs") == 0)
+    if (strcmp(category, "perf") == 0)
         group = ImGuiTestGroup_Perfs;
 
     ImGuiTest* t = IM_NEW(ImGuiTest)();