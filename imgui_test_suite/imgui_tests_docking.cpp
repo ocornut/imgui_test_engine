8dda6c1f4deeff2eca5fa11d7e21a0a21129655e
ocornut <omarcornut@gmail.com>
2023-12-05 12:21:37 +0100

    TestSuite: use ==/!= courtesy operators that are now available. (docking tests)

--- imgui_test_suite/imgui_tests_docking.cpp
+++ imgui_test_suite/imgui_tests_docking.cpp
@@ -133,7 +133,9 @@ static void DockingTestsGenericGuiFunc(ImGuiTestContext* ctx)
         }
 }
 
+#if IMGUI_VERSION_NUM < 19002
 static inline bool operator==(const ImVec2& lhs, const ImVec2& rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }    // for IM_CHECK_EQ()
+#endif
 #endif
 
 //-------------------------------------------------------------------------
