399114c1edc3ce80160447ed90b657e9d72ef8ea
ocornut <omarcornut@gmail.com>
2024-01-16 12:08:36 +0100

    TestSuite: added helper build_win64.bat build script alternative. (#40)

--- imgui_test_suite/build_win64.bat
+++ imgui_test_suite/build_win64.bat
@@ -1,4 +1,4 @@
-@REM Minimal build script for building app_mimimal with MSVC (cl.exe), x64 build
+@REM Minimal build script for building imgui_test_suite with MSVC (cl.exe), x64 build
 @REM You need to run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
 
 @REM NOTE: Requires Visual Studio or Microsoft Build Tools Installed and may require that you update the path to vcvarsall.bat below
@@ -27,22 +27,22 @@ IF "%WindowsSdkDir%" == "" (
 )
 
 @set OUT_DIR=Debug
-@set OUT_EXE=app_minimal.exe
+@set OUT_EXE=imgui_test_suite.exe
 
 IF NOT EXIST %OUT_DIR% mkdir %OUT_DIR%
 
 set CommonCompilerFlags=-Fo%OUT_DIR%\
-set Defines=-DWIN32=1 -DIMGUI_USER_CONFIG=\"app_minimal/app_minimal_imconfig.h\" -DUNICODE
+set Defines=-DWIN32=1 -DIMGUI_USER_CONFIG=\"imgui_test_suite/imgui_test_suite_imconfig.h\" -DUNICODE /D _UNICODE
 set LinkerLibs=d3d11.lib d3dcompiler.lib dxgi.lib shell32.lib
 set CommonLinkerFlags= -incremental %LinkerLibs%
 
 set INCLUDES=-I "%WindowsSdkDir%Include\um" -I "%WindowsSdkDir%Include\shared"
-set INCLUDES=%INCLUDES% -I.. -I..\..\imgui -I..\..\imgui\backends -I..\shared
+set INCLUDES=%INCLUDES% -I.. -I..\..\imgui -I..\..\imgui\backends -I..\shared -Ithirdparty\implot
 
-set SOURCES=*.cpp ..\imgui_test_engine\imgui*.cpp ..\..\imgui\imgui*.cpp ..\shared\imgui_app.cpp
+set SOURCES=*.cpp ..\imgui_test_engine\imgui*.cpp ..\..\imgui\imgui*.cpp ..\shared\imgui_app.cpp thirdparty/implot/*.cpp
 
 set OutputFile=-Fe%OUT_DIR%\%OUT_EXE%
-set MapFile=-Fm%OUT_DIR%\app_minimal.map
+set MapFile=-Fm%OUT_DIR%\imgui_test_suite.map
 
 cl %OutputFile% %CommonCompilerFlags% %Defines% %INCLUDES% %MapFile% %SOURCES% /link %CommonLinkerFlags%
 
