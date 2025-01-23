ocornut/imgui_test_engine

commit bdfd9868a7a85296185c48e652117a3623d65053

TestEngine: registering/unregistered tests during runtime ensure their source line ranges is up to date.

imgui_test_engine/imgui_te_engine.cpp
@@ -64,7 +64,6 @@ static ImGuiTestEngine* GImGuiTestEngine = nullptr;
 static void ImGuiTestEngine_BindImGuiContext(ImGuiTestEngine* engine, ImGuiContext* ui_ctx);
 static void ImGuiTestEngine_UnbindImGuiContext(ImGuiTestEngine* engine, ImGuiContext* ui_ctx);
 static void ImGuiTestEngine_CoroutineStopAndJoin(ImGuiTestEngine* engine);
-static void ImGuiTestEngine_StartCalcSourceLineEnds(ImGuiTestEngine* engine);
 static void ImGuiTestEngine_ClearInput(ImGuiTestEngine* engine);
 static void ImGuiTestEngine_ApplyInputToImGuiContext(ImGuiTestEngine* engine);
 static void ImGuiTestEngine_ProcessTestQueue(ImGuiTestEngine* engine);
@@ -265,7 +264,6 @@ void    ImGuiTestEngine_Start(ImGuiTestEngine* engine, ImGuiContext* ui_ctx)
 
     engine->UiContextTarget = ui_ctx;
     ImGuiTestEngine_BindImGuiContext(engine, engine->UiContextTarget);
-    ImGuiTestEngine_StartCalcSourceLineEnds(engine);
 
     // Create our coroutine
     // (we include the word "Main" in the name to facilitate filtering for both this thread and the "Main Thread" in debuggers)
@@ -280,9 +278,11 @@ void    ImGuiTestEngine_Start(ImGuiTestEngine* engine, ImGuiContext* ui_ctx)
 void    ImGuiTestEngine_Stop(ImGuiTestEngine* engine)
 {
     IM_ASSERT(engine->Started);
+    IM_ASSERT(engine->UiContextTarget != NULL);
 
     engine->Abort = true;
     ImGuiTestEngine_CoroutineStopAndJoin(engine);
+    ImGuiTestEngine_UnbindImGuiContext(engine, engine->UiContextTarget);
     ImGuiTestEngine_Export(engine);
     engine->Started = false;
 }
@@ -1244,6 +1244,7 @@ ImGuiTest* ImGuiTestEngine_RegisterTest(ImGuiTestEngine* engine, const char* cat
     t->SourceFile = src_file;
     t->SourceLine = t->SourceLineEnd = src_line;
     engine->TestsAll.push_back(t);
+    engine->TestsSourceLinesDirty = true;
 
     return t;
 }
@@ -1270,6 +1271,7 @@ void ImGuiTestEngine_UnregisterTest(ImGuiTestEngine* engine, ImGuiTest* test)
         engine->UiSelectAndScrollToTest = nullptr;
     if (engine->UiSelectedTest == test)
         engine->UiSelectedTest = nullptr;
+    engine->TestsSourceLinesDirty = true;
 
     IM_DELETE(test);
 }
@@ -1283,6 +1285,7 @@ void ImGuiTestEngine_UnregisterAllTests(ImGuiTestEngine* engine)
     engine->TestsQueue.clear();
     engine->UiSelectAndScrollToTest = nullptr;
     engine->UiSelectedTest = nullptr;
+    engine->TestsSourceLinesDirty = true;
 }
 
 ImGuiPerfTool* ImGuiTestEngine_GetPerfTool(ImGuiTestEngine* engine)
@@ -1418,8 +1421,10 @@ void ImGuiTestEngine_QueueTests(ImGuiTestEngine* engine, ImGuiTestGroup group, c
     }
 }
 
-static void ImGuiTestEngine_StartCalcSourceLineEnds(ImGuiTestEngine* engine)
+// FIXME: This is grossly broken as it assume same filename.
+void ImGuiTestEngine_UpdateTestsSourceLines(ImGuiTestEngine* engine)
 {
+    engine->TestsSourceLinesDirty = false;
     if (engine->TestsAll.empty())
         return;
 

imgui_test_engine/imgui_te_engine.h
@@ -398,15 +398,17 @@ struct IMGUI_API ImGuiTest
     const char*                     Name = nullptr;                 // Literal, generally not owned unless NameOwned=true
     ImGuiTestGroup                  Group = ImGuiTestGroup_Unknown; // Coarse groups: 'Tests' or 'Perf'
     bool                            NameOwned = false;              //
-    const char*                     SourceFile = nullptr;           // __FILE__
-    int                             SourceLine = 0;                 // __LINE__
-    int                             SourceLineEnd = 0;              // Calculated by ImGuiTestEngine_StartCalcSourceLineEnds()
     int                             ArgVariant = 0;                 // User parameter. Generally we use it to run variations of a same test by sharing GuiFunc/TestFunc
     ImGuiTestFlags                  Flags = ImGuiTestFlags_None;    // See ImGuiTestFlags_
     ImFuncPtr(ImGuiTestGuiFunc)     GuiFunc = nullptr;              // GUI function (optional if your test are running over an existing GUI application)
     ImFuncPtr(ImGuiTestTestFunc)    TestFunc = nullptr;             // Test function
     void*                           UserData = nullptr;             // General purpose user data (if assigning capturing lambdas on GuiFunc/TestFunc you may not need to use this)
     //ImVector<ImGuiTestRunTask>    Dependencies;                   // Registered via AddDependencyTest(), ran automatically before our test. This is a simpler wrapper to calling ctx->RunChildTest()
+
+    // Sources information (exposed in UI)
+    const char*                     SourceFile = nullptr;           // __FILE__
+    int                             SourceLine = 0;                 // __LINE__
+    int                             SourceLineEnd = 0;              // end of line (when calculated by ImGuiTestEngine_StartCalcSourceLineEnds())
 
     // Last Test Output/Status
     // (this is the only part that may change after registration)

imgui_test_engine/imgui_te_internal.h
@@ -160,6 +160,7 @@ struct ImGuiTestEngine
     ImVector<ImGuiTest*>        TestsAll;
     ImVector<ImGuiTestRunTask>  TestsQueue;
     ImGuiTestContext*           TestContext = nullptr;          // Running test context
+    bool                        TestsSourceLinesDirty = false;
     ImVector<ImGuiTestInfoTask*>InfoTasks;
     ImGuiTestGatherTask         GatherTask;
     ImGuiTestFindByLabelTask    FindByLabelTask;
@@ -220,6 +221,7 @@ void                ImGuiTestEngine_RunTest(ImGuiTestEngine* engine, ImGuiTestCo
 
 void                ImGuiTestEngine_RebootUiContext(ImGuiTestEngine* engine);
 ImGuiPerfTool*      ImGuiTestEngine_GetPerfTool(ImGuiTestEngine* engine);
+void                ImGuiTestEngine_UpdateTestsSourceLines(ImGuiTestEngine* engine);
 
 // Screen/Video Capturing
 bool                ImGuiTestEngine_CaptureScreenshot(ImGuiTestEngine* engine, ImGuiCaptureArgs* args);

imgui_test_engine/imgui_te_ui.cpp
@@ -22,7 +22,7 @@
 // - DrawTestLog() [internal]
 // - GetVerboseLevelName() [internal]
 // - ShowTestGroup() [internal]
-// - ImGuiTestEngine_ShowTestWindows()
+// - ImGuiTestEngine_ShowTestEngineWindows()
 //-------------------------------------------------------------------------
 
 // Look for " filename:number " in the string and add menu option to open source.
@@ -845,6 +845,9 @@ static void ImGuiTestEngine_ShowTestTool(ImGuiTestEngine* engine, bool* p_open)
 
 void    ImGuiTestEngine_ShowTestEngineWindows(ImGuiTestEngine* e, bool* p_open)
 {
+    if (e->TestsSourceLinesDirty)
+        ImGuiTestEngine_UpdateTestsSourceLines(e);
+
     // Test Tool
     ImGuiTestEngine_ShowTestTool(e, p_open);
 

commit 8e06a653cc2b3886e4e036bb628b9bd46cd8c3f8

TestEngine: fixed test engine .ini settings loss (caused by bdfd986)
Didn't mean to commit the ImGuiTestEngine_UnbindImGuiContext() call, it was a test for #67

imgui_test_engine/imgui_te_engine.cpp
@@ -282,7 +282,7 @@ void    ImGuiTestEngine_Stop(ImGuiTestEngine* engine)
 
     engine->Abort = true;
     ImGuiTestEngine_CoroutineStopAndJoin(engine);
-    ImGuiTestEngine_UnbindImGuiContext(engine, engine->UiContextTarget);
+    //ImGuiTestEngine_UnbindImGuiContext(engine, engine->UiContextTarget);
     ImGuiTestEngine_Export(engine);
     engine->Started = false;
 }