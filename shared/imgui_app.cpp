7be88e7fa267f251c7271a33d72b1aa5ca04a4cb
ocornut <omar@miracleworld.net>
2026-05-12 15:58:11 +0200

    imgui_app: fixes for ImDrawList::AddRect() signature changes (docking branch)

--- shared/imgui_app.cpp
+++ shared/imgui_app.cpp
@@ -1607,7 +1607,11 @@ static void ImGuiApp_InstallMockViewportsBackend(ImGuiApp*)
         if (viewport != main_viewport)
         {
             ImRect r = ((ImGuiViewportP*)viewport)->GetMainRect();
+#if IMGUI_VERSION_NUM < 19276
             ImGui::GetForegroundDrawList(main_viewport)->AddRect(r.Min, r.Max, IM_COL32(255, 0, 0, 255), 0.0f, ImDrawFlags_None, 3.0f);
+#else
+            ImGui::GetForegroundDrawList(main_viewport)->AddRect(r.Min, r.Max, IM_COL32(255, 0, 0, 255), 0.0f, 3.0f);
+#endif
         }
     };
     platform_io.Platform_RenderWindow = NULL;
