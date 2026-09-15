2628e39cc0ea3a0a612d5d039543c9d4e873c720
ocornut <omar@miracleworld.net>
2026-09-15 17:06:22 +0200

    TestEngine: fixed build. amends for < 19297. (#102)

--- imgui_test_suite/imgui_tests_docking.cpp
+++ imgui_test_suite/imgui_tests_docking.cpp
@@ -16,6 +16,7 @@
 #include "imgui_impl_null.h"
 #include "imgui_test_engine/imgui_te_engine.h"      // IM_REGISTER_TEST()
 #include "imgui_test_engine/imgui_te_context.h"
+#include "imgui_test_engine/imgui_te_utils.h"
 #include "imgui_test_engine/thirdparty/Str/Str.h"
 
 // Warnings
